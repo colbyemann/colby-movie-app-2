6262e1e6c9f61edf2fbf198771aefa1a2a91f889
Colby Emann <colbyemann2010@gmail.com>
2020-03-27 17:19:31 -0600

    help

--- app.js
+++ app.js
@@ -16,20 +16,13 @@ require('./handlers/dataConnector.js').connect();
 // create an express app
 const app = express();
 
-
 // view engine setup
 app.use(expressLayouts);
 app.set('view engine', 'ejs'); 
 
-
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('/client', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-
+// serves up static files from the public folder. 
+app.use(express.static('client'));
+app.use('/static', express.static('client'));
 
 // setup express middleware
 app.use(parser.json());
@@ -68,7 +61,7 @@ app.use(function(err, req, res, next) {
 });
 
 // Use express to listen to port
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Server now running at port= " + port);
 });
