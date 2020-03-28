6ee30b5bd8446a6f1ce95a7980cd59932ea7faeb
Colby Emann <colbyemann2010@gmail.com>
2020-03-28 17:26:13 -0600

    api test

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -20,6 +20,7 @@ class MovieDetials extends React.Component {
         const jsonData = await response.json();
        console.log(this.props.location.state.id);
         this.setState( {movie: jsonData, loading: false} );
+        console.log(movie)
         
         }
         catch (error) {
