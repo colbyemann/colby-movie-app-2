a31d8e8f58a0f497660829c33e6fc87599b519f4
Colby Emann <colbyemann2010@gmail.com>
2020-03-27 18:10:56 -0600

    try

--- app.js
+++ app.js
@@ -23,12 +23,13 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs'); 
 
 if (process.env.NODE_ENV === 'production') {
- 
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
     
   // Handle React routing, return all requests to React app
-  app.get('/client', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  app.get(['/app', '/app/*'], function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
+   });
 }
 // setup express middleware
 app.use(parser.json());
