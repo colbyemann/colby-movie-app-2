6ec3d66472ef80ed06adb2ee2a640a53b22c6623
Colby Emann <colbyemann2010@gmail.com>
2020-03-27 19:03:11 -0600

    why lord

--- app.js
+++ app.js
@@ -52,15 +52,6 @@ app.use('/', openRoutes);
 const apiRoutes = require('./handlers/apiRouter.js');
 app.use('/api', apiRoutes );
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use('/client', express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({ error : err });
