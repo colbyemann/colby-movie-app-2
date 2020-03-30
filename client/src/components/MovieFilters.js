250653f3225ed39ba8982ffaec6f27db565e46b7
Colby Emann <colbyemann2010@gmail.com>
2020-03-30 12:51:10 -0600

    push failed

--- client/src/components/MovieFilters.js
+++ client/src/components/MovieFilters.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './MovieFilters.css';
 // Using an ES6 transpiler like Babel
-
+import { Input } from 'antd';
 import { Button } from 'antd';
-
+import { Radio } from 'antd';
+import Slider from 'react-rangeslider';
 import { Drawer, Button } from 'antd';
  
 // To include the default styles
@@ -165,9 +166,73 @@ class MovieFilters extends React.Component {
           style={{ position: 'absolute' }}
         >
           <p>Some contents...</p>
+          <Slider
+          min={0}
+          max={10}
+          step={0.1}
+          value={Math.round( this.state.valuebetween1 * 10 ) / 10}
+          onChange={this.sliderChange3}
+        />
         </Drawer>
       </div>
+    {/* <form className="photoForm">
+    <legend>Movie Filters</legend>
+    <label>Title</label>
+    <Input type='text' name='title'onChange={this.handleChange}/>
+    <label>Year</label>
+    <label for="before"><Radio type="radio" id="before" name="year" value="option1" checked={this.state.selectedOption === 'option1'} 
+                      onChange={this.handleOptionChange}/>Before <Input type='text' name='beforetext' onChange={this.handleChange}/></label>
+    <label for="after"><Radio type="radio" id="after" name="year" value="option2" checked={this.state.selectedOption === 'option2'} 
+                      onChange={this.handleOptionChange}/>After <Input type='text' name='aftertext' onChange={this.handleChange}/></label>
+    <label for="between1"><Radio type="radio" id="betwen1" name="year" value="option3" checked={this.state.selectedOption === 'option3'} 
+                      onChange={this.handleOptionChange}/>Between <Input type='text' name='betweentext1' onChange={this.handleChange}/><Input type='text' name='betweentext2' onChange={this.handleChange}/></label>
+
+<label>Rating</label>
+    <label for="below"><Radio type="radio" id="below" name="rating" value="option1" checked={this.state.selectedOption2 === 'option1'} 
+                      onChange={this.handleOptionChangeRating}/>Below <div className='slider'>
+        <Slider
+          min={0}
+          max={10}
+          step={0.1}
+          value={Math.round( this.state.valuebelow * 10 ) / 10}
+          onChange={this.sliderChange1}
+        />
+      </div></label>
+    <label for="above"><Radio type="radio" id="above" name="rating" value="option2" checked={this.state.selectedOption2 === 'option2'} 
+                      onChange={this.handleOptionChangeRating}/>Above <div className='slider'>
+        <Slider
+          min={0}
+          max={10}
+          step={0.1}
+          value={Math.round( this.state.valueabove * 10 ) / 10}
+          onChange={this.sliderChange2}
+        />
+      </div></label>
+    <label for="between2"><Radio type="radio" id="between2" name="rating" value="option3" checked={this.state.selectedOption2 === 'option3'} 
+                      onChange={this.handleOptionChangeRating}/>Between <div className='slider'>
+        <Slider
+          min={0}
+          max={10}
+          step={0.1}
+          value={Math.round( this.state.valuebetween1 * 10 ) / 10}
+          onChange={this.sliderChange3}
+        />
+      </div>
+      <div className='slider'>
+        <Slider
+          min={0}
+          max={10}
+          step={0.1}
+          value={Math.round( this.state.valuebetween2 * 10 ) / 10}
+          onChange={this.sliderChange4}
+        />
+      </div>
+      </label>
+
     
+    <Button onClick={(e) => {this.handleClick(e)}}>Filter</Button>
+    <Button >Clear</Button>
+    </form> */}
     </div>
     
     </aside>
